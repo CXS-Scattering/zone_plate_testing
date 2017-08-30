89da4f65dc6595a886a02049d175b5501aee65f8
Sajid Ali <30510036+sajid-ali-nu@users.noreply.github.com>
2017-08-30 16:30:04 -0500

    added some routines

--- create_disc.py
+++ create_disc.py
@@ -7,7 +7,7 @@ Created on Wed Aug 30 11:17:12 2017
 
 import numpy as np
 import matplotlib.pyplot as plt
-import urllib
+
 
 def plot_region(sides_x,sides_y,r,step,n):
      plt.plot([sides_x[0],sides_x[0]],[sides_y[0],sides_y[1]],'g-')
@@ -48,30 +48,16 @@ def create_disc(X,Y,step,r,n):
             sides_y = np.array([y1-step/2,y1+step/2])
             Z1[i][j] = get_partial_fill(sides_x,sides_y,r,step,n)
     return Z1
-def get_property(mat,energy):
-    url = "http://henke.lbl.gov/cgi-bin/pert_cgi.pl"
-    data = {'Element':str(mat), 'Energy':str(energy), 'submit':'Submit Query'}
-    data = urllib.parse.urlencode(data)
-    data = data.encode('utf-8')
-    req = urllib.request.Request(url, data)
-    resp = urllib.request.urlopen(req)
-    respDat = resp.read()
-    response = respDat.split()
-    d =  b'g/cm^3<li>Delta'
-    i = response.index(d)
-    delta = str(response[i+2])[:str(response[i+2]).index('<li>Beta')][2:]
-    beta = str(response[i+4])[2:-1]
-    return float(delta),float(beta)
-    
-N = 250
+
+'''    
+N = 100
 n = 50
 zp = np.zeros((N,N))
 x =  np.linspace(-2.5,2.5,N)
 X,Y = np.meshgrid(x,x)
 step = x[-1]-x[-2]
-disc = create_disc(X,Y,step,2,n)
-#plt.imshow(disc,extent=[X[0][0],X[0][-1],Y[1][0],Y[-1][0]])
-
-mat = 'Au'
-energy = 5000
-delta,beta = get_property(mat,energy)
+disc1 = create_disc(X,Y,step,1.5,n)
+disc2 = create_disc(X,Y,step,2,n)
+disc = disc2 - disc1
+plt.imshow(disc,extent=[X[0][0],X[0][-1],Y[1][0],Y[-1][0]])
+'''
