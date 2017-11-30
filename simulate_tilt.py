b951a76ee26ac9bcfc3d3b5d7c7acf8bc15ec832
sajid-ali-nu <sajidsyed2021@u.northwestern.edu>
2017-11-30 11:30:38 -0600

    modified:   .ipynb_checkpoints/create_zp-checkpoint.ipynb 	modified:   create_zp.ipynb 	modified:   simulate_tilt.py

--- simulate_tilt.py
+++ simulate_tilt.py
@@ -4,18 +4,31 @@ from skimage import io,img_as_float
 import time
 from multislice import prop,prop_utils
 
+def find_edge(x):
+    if x<7500:
+        if x>100:
+            return 100
+        else :
+            return int(np.floor(x / 2) * 2)
+    else :
+        if (15000-x)>100:
+            return 100
+        else :
+            return int(np.floor(15000-x / 2) * 2)
+
 def get_focal_spot(focal_plane_):
-    focal_spot = np.zeros((200,200))
     x_,y_ = np.where(focal_plane_==np.max(focal_plane_))
     x_ = x_[0]
     y_ = y_[0]
-    if (x_ > 100) & (y_>100):
-        n = 100
-        focal_spot = focal_plane_[x_-n:x_+n,y_-n:y_+n]
-    else : 
-        n = 50
-        focal_spot[50:150,50:150] = focal_plane_[x_-n:x_+n,y_-n:y_+n]
-    return focal_spot
+    print(x_,y_)
+    x1 = find_edge(x_)
+    y1 = find_edge(y_)
+    focal_spot_ = np.zeros((200,200))
+    if (x1+y1) != 200:
+        focal_spot_[100-x1:100+x1,100-y1:100+y1] = focal_plane_[x_-x1:x_+x1,y_-y1:y_+y1]
+    else :
+        focal_spot_[:,:] = focal_plane_[x_-100:x_+100,y_-100:y_+100]
+    return focal_spot_,x_,y_,np.max(focal_plane_)
 
 
 def tilt(i):
@@ -30,12 +43,13 @@ def tilt(i):
     number_of_steps_zp =  prop_utils.number_of_steps(step_xy,wavel,zp_thickness) + 1
     wave_focus = prop_utils.propogate_through_object(wave_in,zp_delta,zp_beta,zp_thickness,step_xy,wavel,L,
                                                      number_of_steps_zp,d1=0,d2=f, xray_object='zone Plate')
-    focal_spot = get_focal_spot(np.abs(wave_focus))
-    io.imsave('focal_spot_tile_anlge'+str(i)+'.tiff',img_as_float(focal_spot)
+    focal_spot,x_,y_,max_val = get_focal_spot(np.abs(wave_focus))
+    io.imsave('tilt_'+str(i)+'.tiff',img_as_float(focal_spot))
     t2 = time.time()
     print('tilt image number :',i,'time taken :',(t2-t1))
-
-
+    print(x_,y_,max_val)
+    return {'tilt_angle':i,'max_loc':np.array([x_,y_]),'max_val':max_val}
+    
 zp = np.load('Au_417_zp.npy')
 parameters = pickle.load(open('parameters_Au_417.pickle','rb'))
 zp_coords = parameters['zp_coords']    
@@ -51,6 +65,7 @@ zp_thickness = 1e-6
 print('all imports done')
 time.sleep(1)
 inputs = np.linspace(-1,1,50)
+max_loc = []
 for i in inputs :
-    tilt(i)
-
+    max_loc.append(tilt(angle,zp,t,parameters,out_dir))
+np.save('max_loc.npy', max_loc) 
